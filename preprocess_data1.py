c0e200a859b7febb481478c39c8a9b4d9ff52561
chihchunchen <chihchunchen>
2015-03-01 22:28:19 +0000

    camel case

--- preprocess_data1.py
+++ preprocess_data1.py
@@ -7,16 +7,19 @@ from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
 
 #From: https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
-filename = "wdbc.csv"
+fileName = "wdbc.csv"
+fileOpen = open(fileName, "rU")
+csvData = csv.reader(fileOpen)
+dataList = list(csvData)
 
 
 #Labels for the outputs
 output_labels = ["B", "M"]
 
-data = list(csv.reader(open(filename, 'rU' )))
-data =  scipy.array(data)
-X = data[0:,2:32]
-y = data[0:, 1]
+
+dataArray =  scipy.array(dataList)
+X = dataArray[0:,2:32]
+y = dataArray[0:, 1]
 
 
 #Check all data has been successfully loaded (array shape):
@@ -39,7 +42,7 @@ le = preprocessing.LabelEncoder()
 #Fit numerical values to the categorical data.
 le.fit(y)
 #Transform the labels into numerical correspondents.
-transformed_y = le.transform(y)
+yTransformed = le.transform(y)
 
 #Cast X variables into floats (they are parsed as strings):
 X = X.astype(numpy.float)
@@ -49,36 +52,36 @@ plt.hist(X[:, 1])
 plt.show()
 
 #Normalise variables in X:
-X_normalised = preprocessing.normalize(X, norm="l2")
+XNormalised = preprocessing.normalize(X, axis = 0, norm="l2")
 #Plot normalised:
-plt.hist(X_normalised[:, 1])
+plt.hist(XNormalised[:, 1])
 plt.show()
-plt.hist(X_normalised[:, 2])
+plt.hist(XNormalised[:, 2])
 plt.show()
 #L1 (l1) normalisation is least absolute deviations; L2 (l2, as above) normalisation is least squares.
 
 #Standardise variables in X:
-X_standardised = preprocessing.scale(X)
+XStandardised = preprocessing.scale(X, axis = 0)
 #Plot standardised:
-plt.hist(X_standardised[:, 1])
+plt.hist(XStandardised[:, 1])
 plt.show()
-plt.hist(X_standardised[:, 2])
+plt.hist(XStandardised[:, 2])
 plt.show()
 #Standardisation with scale(function) - mean 0 sd 1.
 #Assumed by many algorithms so best practice to do it.
 
 #Split into training and test data:
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+XTrain, XTest, yTrain, yTest = train_test_split(X, y)
 #Check:
-print 'X_train dimensions: ', X_train.shape
-print 'y_train dimensions: ', y_train.shape
-print 'X_test dimensions: ', X_test.shape
-print 'y_test dimensions: ', y_test.shape
+print 'X train dimensions: ', XTrain.shape
+print 'y train dimensions: ', yTrain.shape
+print 'X test dimensions: ', XTest.shape
+print 'y test dimensions: ', yTest.shape
 
 
 #Plot heatmap of correlations between different features:
-correlation_matrix = numpy.corrcoef(X, rowvar=0)
+correlationMatrix = numpy.corrcoef(X, rowvar=0)
 fig, ax = plt.subplots()
-heatmap = ax.pcolor(correlation_matrix, cmap=plt.cm.Blues)
+heatmap = ax.pcolor(correlationMatrix, cmap=plt.cm.Blues)
 plt.show()
 
